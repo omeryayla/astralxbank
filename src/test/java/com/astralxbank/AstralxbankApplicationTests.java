1c940151954e0365217d7ec25c1762bbfc82c078
Ömer Faruk Yayla <omerfruk.yayla@gmail.com>
2025-06-11 21:23:18 +0300

    Initial project structure: Spring Boot setup, SecurityConfig added

---
+++ src/test/java/com/astralxbank/AstralxbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astralxbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstralxbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
